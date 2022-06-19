e98f90b09a6a0112015dd7628c7e10ae479ee6a8
reja ahmed <reja6402@gmail.com>
2022-06-19 15:30:34 +0600

    Single Post Read

--- api/post/read.php
+++ api/post/read.php
@@ -1,52 +1,51 @@
 <?php 
-  // Headers
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
-  include_once '../../config/Database.php';
-  include_once '../../models/Post.php';
+    include_once '../../config/Database.php';
+    include_once '../../models/Post.php';
 
-  // Instantiate DB & connect
-  $database = new Database();
-  $db = $database->connect();
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
 
-  // Instantiate blog post object
-  $post = new Post($db);
+    // Instantiate blog post object
+    $post = new Post($db);
 
-  // Blog post query
-  $result = $post->read();
-  // Get row count
-  $num = $result->rowCount();
+    // Blog post query
+    $result = $post->read();
 
-  // Check if any posts
-  if($num > 0) {
-    // Post array
-    $posts_arr = array();
-    // $posts_arr['data'] = array();
+    // Get row count
+    $num = $result->rowCount();
 
-    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      extract($row);
+    // Check if any posts
+    if($num > 0) {
+            // Post array
+            $posts_arr = array();
+            $posts_arr['data'] = array();
 
-      $post_item = [
-        'id' => $id,
-        'title' => $title,
-        'body' => html_entity_decode($body),
-        'author' => $author,
-        'category_id' => $category_id,
-        'category_name' => $category_name
-      ];
+            while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
 
-      // Push to "data"
-      array_push($posts_arr, $post_item);
-      // array_push($posts_arr['data'], $post_item);
+            $post_item = [
+                'id' => $id,
+                'title' => $title,
+                'body' => html_entity_decode($body),
+                'author' => $author,
+                'category_id' => $category_id,
+                'category_name' => $category_name
+            ];
+
+            // Push to "data"
+            //   array_push($posts_arr, $post_item);
+            array_push($posts_arr['data'], $post_item);
+        }
+
+        // Turn to JSON & output
+        echo json_encode($posts_arr);
+
+    } else {
+        // No Posts
+        echo json_encode(array('message' => 'No Posts Found'));
     }
-
-    // Turn to JSON & output
-    echo json_encode($posts_arr);
-
-  } else {
-    // No Posts
-    echo json_encode(
-      array('message' => 'No Posts Found')
-    );
-  }
