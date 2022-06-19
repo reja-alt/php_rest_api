7a5bc632516a9afef7d89c8dfcc07c052d0666c6
reja ahmed <reja6402@gmail.com>
2022-06-19 17:12:05 +0600

    post created

--- models/Post.php
+++ models/Post.php
@@ -67,4 +67,41 @@ class Post {
          $this->category_id = $row['category_id'];
          $this->category_name = $row['category_name'];
     }
+
+    //Create Data
+    public function Create() {
+        //create query
+        $query = 'INSERT INTO '. $this->table .'
+                SET
+                title = :title,
+                body = :body,
+                author = :author,
+                category_id  = :category_id';
+
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //clean data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        //bindParam
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+
+        // Execute query
+        if($stmt->execute()) {
+            return true;
+        }
+
+        // Print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+
 }
